11f5fcca1425f46f6da86b45a4c11ca53e3c84c6
sungkpark <SKP@Sungkyungs-MacBook-Pro.local>
2018-12-06 02:37:30 +0100

    better implementation of move (dice)

--- public/javascripts/ui-right.js
+++ public/javascripts/ui-right.js
@@ -7,6 +7,24 @@ function moveOneGridAfter49(){
   currentPosition--;
 }
 
+function move(numRolled){
+    var before49 = true;
+    for(var i=0; i<numRolled; i++){
+      if(before49){
+        moveOneGridBelow49();
+      }
+      else{
+        moveOneGridAfter49();
+      }
+      if(currentPosition===49){
+        before49=false;
+      }
+    }
+    if(currentPosition===49){
+      alert("Congrats, you won!");
+    }
+}
+
 /*
  * Object representing the status bar.
  */
